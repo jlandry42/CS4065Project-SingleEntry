7406459b79fda1fb9619bb63da04bdb291d8ca31
jkan635 <jonathan.k.kan@gmail.com>
2019-02-23 23:42:22 -0400

    Updates

---
+++ precache-manifest.7e978761cf5dbd349e64b3db8b567706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87a0339ba1c11edf6f25",
+    "url": "/CS4065Project-SingleEntry/static/js/runtime~main.87a0339b.js"
+  },
+  {
+    "revision": "b71dec15c567b7ee1381",
+    "url": "/CS4065Project-SingleEntry/static/js/main.b71dec15.chunk.js"
+  },
+  {
+    "revision": "39a8a7bc3437c02db6ea",
+    "url": "/CS4065Project-SingleEntry/static/js/2.39a8a7bc.chunk.js"
+  },
+  {
+    "revision": "b71dec15c567b7ee1381",
+    "url": "/CS4065Project-SingleEntry/static/css/main.9643448f.chunk.css"
+  },
+  {
+    "revision": "39a8a7bc3437c02db6ea",
+    "url": "/CS4065Project-SingleEntry/static/css/2.ea0ee175.chunk.css"
+  },
+  {
+    "revision": "b0b46a0e49eb63f535cbc318f558f983",
+    "url": "/CS4065Project-SingleEntry/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/CS4065Project-SingleEntry/precache-manifest.7e978761cf5dbd349e64b3db8b567706.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/CS4065Project-SingleEntry/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
